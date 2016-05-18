ddafe17cf9fd2302f9c16609942a9fe3e990b364
Kevin SeungWhan Chung <schung58@illinois.edu>
2016-05-18 00:08:37 -0500

    add BCindex, change TTA

--- modMesh.f90
+++ modMesh.f90
@@ -31,6 +31,8 @@ contains
 				this%dx = L/ng
 			case(1)						!Dirichlet-Dirichlet
 				this%dx = L/(ng-1)
+			case(2)						!Dirichlet-Neumann
+				this%dx = L/(ng-1)
 		end select
 
 		allocate(this%E(ng))
@@ -72,6 +74,8 @@ contains
 				call solveMesh_periodic(this,eps)
 			case(1)
 				call solveMesh_D_D(this,eps)
+			case(2)
+				call solveMesh_D_N(this,eps)
 		end select
 	end subroutine
 
@@ -101,13 +105,19 @@ contains
 	subroutine solveMesh_D_N(this,eps)						!D(i=1), N(i=N)
 		type(mesh), intent(inout) :: this
 		real(mp), intent(in) :: eps
-		real(mp), dimension(this%ng-1) :: rhs, phi1
+		real(mp), dimension(this%ng-1) :: rhs, phi1, co1, co2, co3
+		co1 = 1.0_mp/this%dx/this%dx
+		co2 = -2.0_mp/this%dx/this%dx
+		co3 = 1.0_mp/this%dx/this%dx
+		co2(this%ng-1) = 1.0_mp/this%dx
+		co1(this%ng-1) = -1.0_mp/this%dx
 
-		rhs = -this%rho(2:this%ng)/eps
-		call TTA(K_DN,phi1,rhs,this%dx)
-		this%phi(2:this%ng-1) = phi1
+		rhs(1:this%ng-2) = -( this%rho(2:this%ng-1) + this%rho_back(2:this%ng-1) )/eps
+		rhs(this%ng-1) = ( this%rho_back(this%ng) + 0.5_mp*this%dx*this%rho(this%ng) )/eps
+!		call TTA(K_DN,phi1,rhs,this%dx)
+		call solve_tridiag(co1,co2,co3,rhs,phi1,this%ng-1)
+		this%phi(2:this%ng) = phi1
 		this%phi(1) = 0.0_mp
-		this%phi(this%ng) = 0.0_mp
 	end subroutine
 
 end module
