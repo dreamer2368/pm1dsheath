95ebbe5b91dd8659098615ff233cd9cb91a30e5b
Kevin SeungWhan Chung <schung58@illinois.edu>
2016-05-22 20:44:47 -0500

    Source: Uniform_Rayleigh

--- modSource.f90
+++ modSource.f90
@@ -9,15 +9,15 @@ contains
 
 !======= [Spatial]_[Velocity] source distribution ==================
 
-	!A(4) number of particle per time step, Uniform on x\in[(0.5-A(1))*L, (0.5+A(1))*L], Maxwellian on v with \sigma = A(2),A(3) (electron,ion)
-	subroutine PartialUniform_Maxwellian(pm)
+	!Keep A(4) number of ion, Uniform on x\in[(0.5-A(1))*L, (0.5+A(1))*L], Rayleigh on v with \sigma = A(2),A(3) (electron,ion)
+	subroutine PartialUniform_Rayleigh(pm)
 		type(PM1D), intent(inout) :: pm
 		integer :: i,nseed,clock
 		integer, allocatable :: seed(:)
 		integer :: Nadd, newN
 		real(mp), allocatable :: xp_add(:), vp_add(:,:), temp_x(:), temp_v(:)
 
-		Nadd = floor(pm%A0(4))
+		Nadd = floor(pm%A0(4)-pm%p(2)%np)
 		allocate(xp_add(Nadd))
 		allocate(vp_add(2,Nadd))
 
@@ -29,7 +29,48 @@ contains
 		deallocate(seed)
 
 		call RANDOM_NUMBER(xp_add)
-		xp_add = xp_add*2.0_mp*pm%A0(1)*pm%L + 0.5_mp*pm%L
+		xp_add = xp_add*2.0_mp*pm%A0(1)*pm%L + (0.5_mp - pm%A0(1))*pm%L
+		vp_add(1,:) = pm%A0(2)*randr(Nadd)
+		vp_add(2,:) = pm%A0(3)*randr(Nadd)
+
+		do i=1,pm%n
+			newN = pm%p(i)%np+Nadd
+			allocate(temp_x(newN))
+			allocate(temp_v(newN))
+			temp_x(1:pm%p(i)%np) = pm%p(i)%xp
+			temp_x(pm%p(i)%np+1:newN) = xp_add
+			temp_v(1:pm%p(i)%np) = pm%p(i)%vp
+			temp_v(pm%p(i)%np+1:newN) = vp_add(i,:)
+
+			call destroySpecies(pm%p(i))
+			call setSpecies(pm%p(i),newN,temp_x,temp_v)
+
+			deallocate(temp_x)
+			deallocate(temp_v)
+		end do
+	end subroutine
+
+	!Keep A(4) number of ion, Uniform on x\in[(0.5-A(1))*L, (0.5+A(1))*L], Maxwellian on v with \sigma = A(2),A(3) (electron,ion)
+	subroutine PartialUniform_Maxwellian(pm)
+		type(PM1D), intent(inout) :: pm
+		integer :: i,nseed,clock
+		integer, allocatable :: seed(:)
+		integer :: Nadd, newN
+		real(mp), allocatable :: xp_add(:), vp_add(:,:), temp_x(:), temp_v(:)
+
+		Nadd = floor(pm%A0(4)-pm%p(2)%np)
+		allocate(xp_add(Nadd))
+		allocate(vp_add(2,Nadd))
+
+		call RANDOM_SEED(size=nseed)
+		allocate(seed(nseed))
+		call SYSTEM_CLOCK(COUNT=clock)
+		seed = clock + 59*(/ ( i-1, i=1,nseed ) /)
+		call RANDOM_SEED(put=seed)
+		deallocate(seed)
+
+		call RANDOM_NUMBER(xp_add)
+		xp_add = xp_add*2.0_mp*pm%A0(1)*pm%L + (0.5_mp - pm%A0(1))*pm%L
 		vp_add(1,:) = pm%A0(2)*randn(Nadd)
 		vp_add(2,:) = pm%A0(3)*randn(Nadd)
 
--- random.f90
+++ random.f90
@@ -24,4 +24,23 @@ contains
 		x = sqrt( -2.0_mp*log(r1) )*cos( 2.0_mp*pi*r2 )
 	end function
 
+	function randr(N) result(x)
+		integer, intent(in) :: N
+		integer :: i,nseed,clock
+		integer, allocatable :: seed(:)
+		real(mp) :: r1(N/2), r2(N-N/2), x(N)
+
+		call RANDOM_SEED(size=nseed)
+		allocate(seed(nseed))
+		call SYSTEM_CLOCK(COUNT=clock)
+		seed = clock + 37*(/ ( i-1, i=1,nseed ) /)
+		call RANDOM_SEED(put=seed)
+		deallocate(seed)
+
+		call RANDOM_NUMBER(r1)
+		call RANDOM_NUMBER(r2)
+		x(1:N/2) = sqrt( -2.0_mp*log(r1) )
+		x(N/2+1:N) = -sqrt( -2.0_mp*log(r2) )
+	end function
+
 END MODULE random
